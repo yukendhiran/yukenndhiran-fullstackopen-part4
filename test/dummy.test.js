fe502cadc82bf6f04045fd1b09fbf93b627cc314
Yukendhiran <yukendhiran4@gmail.com>
2024-07-21 20:00:18 +0530

    Replace Jest with Node:test, 4.5

--- test/dummy.test.js
+++ test/dummy.test.js
@@ -1,8 +1,10 @@
-const listHelper = require('../utils/list_helper')
+const { test, describe } = require("node:test");
+const assert = require("node:assert");
+const listHelper = require("../utils/list_helper");
 
-test('dummy returns one', () => {
-  const blogs = []
+test("dummy returns one", () => {
+  const blogs = [];
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
-})
+  const result = listHelper.dummy(blogs);
+  assert.strictEqual(result, 1);
+});
